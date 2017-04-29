ee48ab76e844fcd81804810f297355db1c4062ff
Ahmed Albishri <ahmad.al.bishri@gmail.com>
2017-04-28 22:48:54 -0500

    new change

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
 from future.standard_library import install_aliases
 install_aliases()
@@ -18,18 +20,15 @@ app = Flask(__name__)
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
 
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
-
-#response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e.c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text='+text+'&lang=en-'+langueges[lang])
 
 def processRequest(req):
 
@@ -38,24 +37,29 @@ def processRequest(req):
     word = parameters.get("text")
     targetlang = parameters.get("targetlang")
 
-
-    #if req.get("result").get("action") != "translate.text":
-     #   return {}
-    #baseurl = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e.c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text='+word+'&lang=en-'+targetlang
-
     response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e.c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=' + word + '&lang=en-' +targetlang)
 
-    #yql_query = makeYqlQuery(req)
-    #if yql_query is None:
-    #    return {}
-    #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-   # result = urlopen(baseurl).read()
-    #data = json.loads(result)
     data = response.json()
-    res = makeWebhookResult(data)
-    return res
 
+    #res = makeWebhookResult(data)
 
+    text = data.get('text')
+    if text is None:
+        return {}
+
+    speech = "Translation is " + text
+
+    #print("Response: Translation is " + text)
+    #print(speech)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "Ahmed"
+    }
+
+    #return res
+
+"""
 def makeWebhookResult(data):
 
     text = data.get('text')
@@ -72,7 +76,7 @@ def makeWebhookResult(data):
         "displayText": speech,
         "source": "Ahmed"
     }
-
+"""
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
