abb73b8fde780b9261f4a2fd6599f2751ee106d0
Ahmed Albishri <ahmad.al.bishri@gmail.com>
2017-04-28 23:16:43 -0500

    new code

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-import requests
+import urllib
 import json
 import os
+import requests
 
 from flask import Flask
 from flask import request
@@ -16,68 +13,66 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
-    #print("Request:")
-    #print(json.dumps(req, indent=4))
+    print("Request:")
+    print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def processRequest(req):
-
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "translate.text":
+        return {}
     result = req.get("result")
     parameters = result.get("parameters")
     word = parameters.get("text")
-    targetlang = parameters.get("targetlang")
+    langto = parameters.get("lang-to")
 
-    response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e.c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=' + word + '&lang=en-' +targetlang)
+    lang = ""
+
+    if langto == "Spanish":
+        lang = "es"
+    elif langto == "Arabic":
+        lang = "ar"
+    elif langto == "French":
+        lang = "fr"
+    elif langto == "Urdu":
+        lang = "ur"
+    else:
+        lang = "en"
+
+    response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e.c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=' + word + '&lang=en-' + lang)
 
     data = response.json()
 
-    #res = makeWebhookResult(data)
+    tr_txt = data['text']
+    #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    text = data.get('text')
-    if text is None:
-        return {}
+    speech = "Translation is " + tr_txt
 
-    speech = "Translation is " + text
-
-    #print("Response: Translation is " + text)
-    #print(speech)
-    return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "Ahmed"
-    }
-
-    #return res
-
-"""
-def makeWebhookResult(data):
-
-    text = data.get('text')
-    if text is None:
-        return {}
-
-    speech = "Translation is " + text
-
-    print("Response: Translation is " + text)
+    print("Response:")
     print(speech)
 
     return {
         "speech": speech,
         "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
         "source": "Ahmed"
     }
-"""
+
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
