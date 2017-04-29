f695dd2c28d708545b0aa2ef16653df6f9dc1be1
AhmedAlbishri <ahmad.al.bishri@gmail.com>
2017-04-28 22:52:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
